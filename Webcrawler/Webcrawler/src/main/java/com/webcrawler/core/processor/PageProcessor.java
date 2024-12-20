596c3e2ad5099c8e6f3085a869e6d96686992d86
Mariia <114170986+mashunchik@users.noreply.github.com>
2024-12-20 09:53:24 +0200

    Delete Webcrawler/Webcrawler directory

--- Webcrawler/Webcrawler/src/main/java/com/webcrawler/core/processor/PageProcessor.java
+++
@@ -1,9 +0,0 @@
-package com.webcrawler.core.processor;
-
-import org.jsoup.nodes.Document;
-
-// Chain of Responsibility Pattern - Handler interface
-public interface PageProcessor {
-    void setNext(PageProcessor nextProcessor);
-    void process(Document document, ProcessingContext context);
-}
